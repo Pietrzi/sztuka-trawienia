17a83c3d369a64db1dbda238ed42c7d08f20f7db
Pietrzi <pietrzi@op.pl>
2020-02-05 18:03:40 +0100

    new content for header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,19 @@ import React from 'react'
 export default function Header() {
     return (
         <div className="header__wrapper">
-            
+            <div className="center-container">
+                <div className="header__content_container">
+                    <div className="header__picture_container">
+                        <div className="header__image"></div>
+                        {/* // <img src="'../../assets/_I1P1973-kopia.jpg'" alt="Danusia rgb clo- Sztuka Trawienia"/> */}
+                    </div>
+                    <div className="header__info_container">
+                        <h1>Wspieram Twoją Równowagę</h1>
+                        <hr/>
+                        <p>Dbam o to aby Twoje pożywienie było lekarstwem dla Twojego ciała, duszi i umysłu</p>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
