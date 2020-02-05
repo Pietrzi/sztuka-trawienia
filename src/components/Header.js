acc7528a2af1dff72d9b46917735034158a210a3
Pietrzi <pietrzi@op.pl>
2020-02-05 19:15:05 +0100

    fighting with pseudo element

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,18 +3,18 @@ import React from 'react'
 export default function Header() {
     return (
         <div className="header__wrapper">
-            <div className="center-container">
-                <div className="header__content_container">
+            <div className="center__container">
+                {/* <div className="header__content_container"> */}
                     <div className="header__picture_container">
                         <div className="header__image"></div>
                         {/* // <img src="'../../assets/_I1P1973-kopia.jpg'" alt="Danusia rgb clo- Sztuka Trawienia"/> */}
                     </div>
                     <div className="header__info_container">
                         <h1>Wspieram Twoją Równowagę</h1>
-                        <hr/>
+                        <div className="title__bottom"></div>
                         <p>Dbam o to aby Twoje pożywienie było lekarstwem dla Twojego ciała, duszi i umysłu</p>
                     </div>
-                </div>
+                {/* </div> */}
             </div>
         </div>
     )
